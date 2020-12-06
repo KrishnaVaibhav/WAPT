ff9ba615c43c24ce08303ed573fba8950655746c
Krishna Vaibhav <59720926+KrishnaVaibhav@users.noreply.github.com>
2020-12-06 12:08:45 +0530

    Add files via upload

---
+++ WAPT/Vulnerabilities/csrf.py
@@ -0,0 +1,10 @@
+def Banner():
+    print('''
+m     m   mm   mmmmm mmmmmmm
+#  #  #   ##   #   "#   #
+" #"# #  #  #  #mmm#"   #
+ ## ##"  #mm#  #        #
+ #   #  #    # #        # 
+ WEB APP PENTESTING TOOLS\n''')
+
+Banner()
